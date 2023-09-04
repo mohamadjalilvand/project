fed2606be6b57e462a3b2889d78ee8cb07c47753
mohamad jalilvand <mhmd.jl1379@gmail.com>
2023-09-04 22:58:17 +0330

    make news view

--- main/views.py
+++ main/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render ,get_object_or_404 ,redirect   
-from .models import Main
+from .models import Main 
+from news.models import News
+
 
 
 # Create your views here.
@@ -10,7 +12,8 @@ def home(request):
     #site = Main.objects.filter(pk=1) روش اول که for لازم دارد
 
     site = Main.objects.get(pk=1)
-    return render(request ,'front/home.html', {'site': site})
+    news= News.objects.all()
+    return render(request ,'front/home.html', {'site': site ,'news':news})
 
 
 def about(request):
